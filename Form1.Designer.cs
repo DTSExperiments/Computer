248c0ea23331d4cd4ff6ae40ab3d633edf7ef05b
Johann Schmid <50572495+Johann-Schmid@users.noreply.github.com>
2022-10-18 13:43:06 +0200

    Add files via upload
    Initial upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿using System.Windows.Forms;
+
+namespace plotBrembs
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plot = new System.Windows.Forms.TabPage();
+            this.debugTextbox = new System.Windows.Forms.TextBox();
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.config = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.serialComboBox = new System.Windows.Forms.ComboBox();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.startSerial = new System.Windows.Forms.Button();
+            this.Threads = new System.Windows.Forms.Button();
+            this.plot.SuspendLayout();
+            this.config.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // plot
+            // 
+            this.plot.Controls.Add(this.debugTextbox);
+            this.plot.Controls.Add(this.formsPlot1);
+            this.plot.Location = new System.Drawing.Point(4, 22);
+            this.plot.Name = "plot";
+            this.plot.Padding = new System.Windows.Forms.Padding(3);
+            this.plot.Size = new System.Drawing.Size(869, 587);
+            this.plot.TabIndex = 0;
+            this.plot.Text = "Plot";
+            this.plot.UseVisualStyleBackColor = true;
+            // 
+            // debugTextbox
+            // 
+            this.debugTextbox.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.debugTextbox.Location = new System.Drawing.Point(471, 3);
+            this.debugTextbox.Name = "debugTextbox";
+            this.debugTextbox.Size = new System.Drawing.Size(395, 20);
+            this.debugTextbox.TabIndex = 2;
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formsPlot1.Location = new System.Drawing.Point(3, 3);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(863, 581);
+            this.formsPlot1.TabIndex = 0;
+            // 
+            // config
+            // 
+            this.config.Controls.Add(this.tableLayoutPanel1);
+            this.config.Location = new System.Drawing.Point(4, 22);
+            this.config.Name = "config";
+            this.config.Padding = new System.Windows.Forms.Padding(3);
+            this.config.Size = new System.Drawing.Size(869, 587);
+            this.config.TabIndex = 1;
+            this.config.Text = "Config";
+            this.config.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.startSerial, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.serialComboBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Threads, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(849, 503);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // serialComboBox
+            // 
+            this.serialComboBox.FormattingEnabled = true;
+            this.serialComboBox.Location = new System.Drawing.Point(3, 3);
+            this.serialComboBox.Name = "serialComboBox";
+            this.serialComboBox.Size = new System.Drawing.Size(418, 21);
+            this.serialComboBox.TabIndex = 0;
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.plot);
+            this.tabControl.Controls.Add(this.config);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(877, 613);
+            this.tabControl.TabIndex = 1;
+            // 
+            // startSerial
+            // 
+            this.startSerial.Location = new System.Drawing.Point(3, 30);
+            this.startSerial.Name = "startSerial";
+            this.startSerial.Size = new System.Drawing.Size(64, 20);
+            this.startSerial.TabIndex = 2;
+            this.startSerial.Text = "Start";
+            this.startSerial.UseVisualStyleBackColor = true;
+            this.startSerial.Click += new System.EventHandler(this.startSerial_Click);
+            // 
+            // Threads
+            // 
+            this.Threads.Location = new System.Drawing.Point(427, 30);
+            this.Threads.Name = "Threads";
+            this.Threads.Size = new System.Drawing.Size(64, 20);
+            this.Threads.TabIndex = 3;
+            this.Threads.Text = "Thread";
+            this.Threads.UseVisualStyleBackColor = true;
+            this.Threads.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 613);
+            this.Controls.Add(this.tabControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.plot.ResumeLayout(false);
+            this.plot.PerformLayout();
+            this.config.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TabPage plot;
+        private ScottPlot.FormsPlot formsPlot1;
+        private TabPage config;
+        private TabControl tabControl;
+        private TextBox debugTextbox;
+        private TableLayoutPanel tableLayoutPanel1;
+        private ComboBox serialComboBox;
+        private Button startSerial;
+        private Button Threads;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,307 @@
+﻿using System.Diagnostics;
+using System.IO.Ports;
+using System.Text;
+using System.Numerics;
+using System.Collections;
+using ScottPlot;
+using System.Windows.Forms;
+using System.Threading;
+using System.Collections.Generic;
+using System.Drawing;
+using System;
+using Timers = System.Timers;
+using ScottPlot.Plottable;
+
+namespace plotBrembs
+{
+    public partial class Form1 : Form
+    {
+        private static Mutex mut = new Mutex();
+
+        Timers.Timer aTimer = null;
+
+        static int _serialClear = 0;
+
+        DateTime beginTime;
+
+        public SerialPort _serialPort = null;
+        public Thread ReadSerialDataThread;
+
+        private double[] liveDataAD = new double[4000];
+        private double[] liveDataPIX = new double[4000];
+
+        private List<byte> list = new List<byte>();
+        private int nextValueIndex = -1;
+
+        SignalPlot signalPlotAD = null;
+        SignalPlot signalPlotPIX = null;
+
+        public delegate void ShowSerialData(List<byte> _readSerialValue);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            List<string> portList = new List<string>();
+
+            portList.AddRange(SerialPort.GetPortNames());
+            portList.Sort();
+
+            string[] portNames = portList.ToArray();
+            serialComboBox.Items.AddRange(portNames);
+
+            serialComboBox.SelectedIndex = 0;
+
+            this.Text = Application.ProductVersion;
+
+            _serialPort = new SerialPort();
+            _serialPort.BaudRate = 115200;
+            _serialPort.Parity = Parity.None;
+
+            signalPlotAD = formsPlot1.Plot.AddSignal(liveDataAD);
+            signalPlotPIX = formsPlot1.Plot.AddSignal(liveDataPIX);
+
+            signalPlotAD.YAxisIndex = 0;
+            signalPlotPIX.YAxisIndex = 1;
+
+            formsPlot1.Plot.SetAxisLimitsX(xMin: 0, xMax: liveDataAD.Length);
+            //formsPlot1.Plot.SetAxisLimits(xMin: 0, xMax: 4000, yMin: -1, yMax: 1, yAxisIndex: 0);
+            //formsPlot1.Plot.SetAxisLimits(yMin: -1, yMax: 800, yAxisIndex: 1);
+
+            //formsPlot1.Plot.YAxis.LockLimits(true);
+            //formsPlot1.Plot.XAxis.LockLimits(true);
+            //formsPlot1.Plot.YAxis2.LockLimits(true);
+
+            formsPlot1.Plot.Title(Application.ProductName);
+            formsPlot1.Plot.Grid(true);
+
+            signalPlotAD.Color = Color.Magenta;
+            signalPlotPIX.Color = Color.Green;
+            signalPlotAD.LineWidth = 2;
+            signalPlotPIX.LineWidth = 2;
+            signalPlotAD.YAxisIndex = 0;
+            signalPlotPIX.YAxisIndex = 1;
+
+            formsPlot1.Plot.YAxis.Color(Color.Magenta);
+            formsPlot1.Plot.YAxis.Label("AD-Value");
+            formsPlot1.Plot.YAxis2.Color(Color.Green);
+            formsPlot1.Plot.YAxis2.Label("PIXEL-Value");
+            formsPlot1.Plot.XAxis.Label("Time");
+
+            formsPlot1.Plot.YAxis2.Ticks(true);
+
+            formsPlot1.Configuration.RightClickDragZoom = false;
+            formsPlot1.Configuration.ScrollWheelZoom = false;
+
+            formsPlot1.Render();
+
+            SetTimer();
+
+            beginTime = DateTime.Now;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //double[] dataX = new double[] { 1, 2, 3, 4, 5 };
+            //double[] dataY = new double[] { 1, 4, 9, 16, 25 };
+            //formsPlot1.Plot.AddScatter(dataX, dataY);
+            //formsPlot1.Refresh();
+            Debug.WriteLine(formsPlot1.Plot.GetAxisLimits().ToString());
+        }
+
+        private void startSerial_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!_serialPort.IsOpen)
+                {
+                    _serialPort.PortName = serialComboBox.Text;
+                    _serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialDataReceivedEventHandler);
+                    _serialPort.Open();
+                    if (_serialPort.BytesToRead == 0)
+                    {
+                        _serialPort.WriteLine("Stop");
+                        _serialPort.WriteLine("Start");
+                    }
+                    else
+                    {
+                        _serialPort.WriteLine("Start");
+                    }
+                    aTimer.Start();
+                    
+                }
+                else
+                {
+                    _serialPort.WriteLine("Stop");
+                    _serialPort.Close();
+                    aTimer.Stop();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Serial failed", "Opening SerialPort Event");
+            }
+
+
+
+        }
+
+        private void SerialDataReceivedEventHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            {
+                try
+                {
+                    SerialPort sp = (SerialPort)sender;
+                    while (sp.BytesToRead > 0)
+                    {
+                        mut.WaitOne();
+                        list.Add(Convert.ToByte(sp.ReadByte()));
+                        mut.ReleaseMutex();
+                    }
+                }
+                catch
+                {
+
+                }
+                this.BeginInvoke(new ShowSerialData(LineReceived), list);
+            }
+        }
+
+        private void LineReceived(List<byte> _serialValue)
+        {
+            ushort nextValueUint = 0;
+            short nextValueAD = 0;
+            ushort nextValuePIX = 0;
+            byte[] byteArray = new byte[4];
+
+            // while (_serialValue.Count >= 5 && _serialValue.IndexOf(0x0A) == 4)
+            try
+            {
+                while (_serialValue.Count >= 5)
+                {
+                    if (_serialValue.IndexOf(0x0A) != 4)
+                    {
+                        throw new Exception("LF not found");
+                    }
+                    Debug.WriteLine(_serialValue.IndexOf(0x0A));
+                    byteArray = _serialValue.GetRange(0, 4).ToArray();
+                    nextValueUint = BitConverter.ToUInt16(byteArray, 0);
+                    BitArray nextValueADBit = new BitArray(byteArray);
+                    if (nextValueADBit[11] == true)
+                    {
+                        nextValueADBit = nextValueADBit.Or(new BitArray(System.BitConverter.GetBytes(0xF000)));
+                    }
+                    nextValueADBit.CopyTo(byteArray, 0);
+                    nextValueAD = BitConverter.ToInt16(byteArray, 0);
+                    nextValuePIX = BitConverter.ToUInt16(byteArray, 2);
+
+                    //Debug.WriteLine(nextValueAD);
+                    //Debug.WriteLine(nextValuePIX);
+
+                    //TimeSpan elapsedTime = new TimeSpan(DateTime.Now.Ticks - beginTime.Ticks);
+
+                    //debugTextbox.Text = elapsedTime.Milliseconds.ToString() + "/ " + nextValueAD.ToString() + "/ " + nextValuePIX.ToString();
+
+                    mut.WaitOne();
+                    _serialValue.RemoveRange(0, 5);
+                    //Debug.WriteLine(_serialValue.Count);
+                    mut.ReleaseMutex();
+
+                    nextValueIndex = (nextValueIndex < liveDataAD.Length - 1) ? nextValueIndex + 1 : 0;
+                    liveDataAD[nextValueIndex] = Convert.ToDouble(nextValueAD);
+                    liveDataPIX[nextValueIndex] = Convert.ToDouble(nextValuePIX);
+
+                    //beginTime = DateTime.Now;
+                }
+            }
+            catch (Exception ex)
+            {
+                _serialClear++;
+                Debug.WriteLine(_serialClear);
+                _serialValue.Clear();
+            }
+
+
+            //Debug.WriteLine(_serialValue.Count);
+            //updateData();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process procces = System.Diagnostics.Process.GetCurrentProcess();
+            System.Diagnostics.ProcessThreadCollection threadCollection = procces.Threads;
+
+            string threads = string.Empty;
+
+            foreach (System.Diagnostics.ProcessThread proccessThread in threadCollection)
+            {
+                threads += string.Format("Thread Id: {0}, ThreadState: {1}\r\n", proccessThread.Id, proccessThread.ThreadState);
+            }
+
+            Debug.WriteLine(Thread.CurrentThread.ManagedThreadId);
+
+
+            MessageBox.Show(threads);
+        }
+
+        private void SetTimer()
+        {
+            aTimer = new Timers.Timer(100);
+            aTimer.Elapsed += this.OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.SynchronizingObject = this;
+            aTimer.Enabled = false;
+        }
+
+        private void OnTimedEvent(Object source, Timers.ElapsedEventArgs e)
+        {
+            formsPlot1.Plot.AxisAutoY(0.1, 0);
+            formsPlot1.Plot.AxisAutoY(0.1, 1);
+            Debug.WriteLine(formsPlot1.Plot.GetAxisLimits().ToString());
+            formsPlot1.Refresh();
+            TimeSpan elapsedTime = new TimeSpan(DateTime.Now.Ticks - beginTime.Ticks);
+            debugTextbox.Text = elapsedTime.Milliseconds.ToString() + " " + nextValueIndex.ToString() + " " + _serialClear.ToString();
+            beginTime = DateTime.Now;
+        }
+
+        private void Form1_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+
+            //System.Text.StringBuilder messageBoxCS = new System.Text.StringBuilder();
+            //messageBoxCS.AppendFormat("{0} = {1}", "CloseReason", e.CloseReason);
+            //messageBoxCS.AppendLine();
+            //messageBoxCS.AppendFormat("{0} = {1}", "Cancel", e.Cancel);
+            //messageBoxCS.AppendLine();
+            //MessageBox.Show(messageBoxCS.ToString(), "FormClosing Event");
+
+            try
+            {
+                if (_serialPort.IsOpen)
+                {
+                    Thread.Sleep(100);
+                    if (_serialPort.BytesToRead > 0)
+                    {
+                        _serialPort.WriteLine("Stop");
+                        _serialPort.Close();
+                    }
+                }
+                else
+                {
+
+                }
+
+                if (aTimer.Enabled == true)
+                {
+                    aTimer.Stop();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "SerialInterface Event");
+            }
+        }
+
+    }
+}
