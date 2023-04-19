a3557b3c36e226243a389becc046cb969ac73a65
Johann Schmid <Johann.Schmid@ur.de>
2023-04-19 16:13:34 +0200

    insert serialInterface class

--- Form1.cs
+++ Form1.cs
@@ -10,7 +10,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System;
 using Timers = System.Timers;
-//using ScottPlot.Plottable;
 using System.Reflection;
 using System.Windows.Forms.DataVisualization.Charting;
 
@@ -21,25 +20,17 @@ namespace plotBrembs
         private static Mutex mut = new Mutex();
         private static System.Threading.Timer simulationTimer = null;
 
-        Timers.Timer aTimer = null;
+        private serialInterface serialCom;
 
-        static int _serialClear = 0;
-
-        DateTime beginTime;
-
-        public SerialPort _serialPort = null;
         public Thread ReadSerialDataThread;
 
         private double[] liveDataAD = new double[1080];
         private double[] liveDataPIX = new double[1080];
 
         private List<byte> list = new List<byte>();
-        private int nextValueIndex = 0;
+        public int nextValueIndex = 0;
 
-        //SignalPlot signalPlotAD = null;
-        //SignalPlot signalPlotPIX = null;
-
-        public delegate void ShowSerialData(List<byte> _readSerialValue);
+        public delegate void ShowSerialData();
 
         Version version = new Version();
 
@@ -48,8 +39,6 @@ namespace plotBrembs
             InitializeComponent();
 
             version = Assembly.GetExecutingAssembly().GetName().Version;
-
-            
 
             List<string> portList = new List<string>();
 
@@ -67,62 +56,14 @@ namespace plotBrembs
 
             this.Text = version.ToString();
 
-            _serialPort = new SerialPort();
-            _serialPort.BaudRate = 115200;
-            _serialPort.Parity = Parity.None;
+            serialCom = new serialInterface();
+            serialCom.OnDataReceived += SerialInterface_OnDataReceived;
 
-            //signalPlotAD = formsPlot1.Plot.AddSignal(liveDataAD);
-            //signalPlotPIX = formsPlot1.Plot.AddSignal(liveDataPIX);
-
-            //formsPlot1.Plot.Benchmark(enable: true);
-
-
-            //signalPlotAD.YAxisIndex = 0;
-            //signalPlotPIX.YAxisIndex = 1;
-
-            //formsPlot1.Plot.SetAxisLimitsX(xMin: 0, xMax: liveDataAD.Length);
-            //formsPlot1.Plot.SetAxisLimits(xMin: 0, xMax: 4000, yMin: -1, yMax: 1, yAxisIndex: 0);
-            //formsPlot1.Plot.SetAxisLimits(xMin: 0, xMax: liveDataAD.Length, yMin: -1, yMax: 800, yAxisIndex: 1);
-
-            ////formsPlot1.Plot.YAxis.LockLimits(true);
-            ////formsPlot1.Plot.XAxis.LockLimits(true);
-            ////formsPlot1.Plot.YAxis2.LockLimits(true);
-
-            //formsPlot1.Plot.Title(Application.ProductName);
-            //formsPlot1.Plot.Grid(true);
-
-            //signalPlotAD.Color = Color.Magenta;
-            //signalPlotPIX.Color = Color.Green;
-            //signalPlotAD.LineWidth = 2;
-            //signalPlotPIX.LineWidth = 2;
-            //signalPlotAD.YAxisIndex = 0;
-            //signalPlotPIX.YAxisIndex = 1;
-
-            //formsPlot1.Plot.YAxis.Color(Color.Magenta);
-            //formsPlot1.Plot.YAxis.Label("AD-Value");
-            //formsPlot1.Plot.YAxis2.Color(Color.Green);
-            //formsPlot1.Plot.YAxis2.Label("PIXEL-Value");
-            //formsPlot1.Plot.XAxis.Label("Time");
-
-            //formsPlot1.Plot.YAxis2.Ticks(true);
-
-            //formsPlot1.Configuration.RightClickDragZoom = false;
-            //formsPlot1.Configuration.ScrollWheelZoom = false;
-
-            //formsPlot1.Render();
-
-            SetTimer();
-
-            beginTime = DateTime.Now;
 
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //double[] dataX = new double[] { 1, 2, 3, 4, 5 };
-            //double[] dataY = new double[] { 1, 4, 9, 16, 25 };
-            //formsPlot1.Plot.AddScatter(dataX, dataY);
-            //formsPlot1.Refresh();
             ChartArea ChartArea0 = new ChartArea("liveData");
             chart1.ChartAreas.Add(ChartArea0);
 
@@ -157,45 +98,17 @@ namespace plotBrembs
 
         private void startSerial_Click(object sender, EventArgs e)
         {
-            try
+            if (serialCom != null)
             {
-                if (!_serialPort.IsOpen)
-                {
-                    _serialPort.PortName = serialComboBox.Text;
-                    _serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialDataReceivedEventHandler);
-                    _serialPort.Open();
-                    if (_serialPort.BytesToRead == 0)
-                    {
-                        _serialPort.WriteLine("Stop");
-                        _serialPort.WriteLine("Start");
-                    }
-                    else
-                    {
-                        _serialPort.WriteLine("Start");
-                    }
-                    aTimer.Start();
-
-                }
-                else
-                {
-                    _serialPort.WriteLine("Stop");
-                    _serialPort.Close();
-                    aTimer.Stop();
-                }
+                serialCom.openPort(serialComboBox.Text);
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Serial failed", "Opening SerialPort Event");
-            }
-
-
-
+           
         }
 
         private void simulateData_Click(object sender, EventArgs e)
         {
             TimerCallback timerCallback = new TimerCallback(sendSimulationData);
-            simulationTimer = new System.Threading.Timer(timerCallback, null, 0, 50);
+            simulationTimer = new System.Threading.Timer(timerCallback, null, 0, 500);
 
 
             Debug.WriteLine("Press the Enter key to exit the program at any time... ");
@@ -203,81 +116,52 @@ namespace plotBrembs
 
         private void sendSimulationData(Object source)
         {
-            Debug.WriteLine("The Elapsed event was raised at {0}", DateTime.Now);
-            mut.WaitOne();
-            //list.Add(Convert.ToByte(0xFF));
-            //list.Add(Convert.ToByte(0x07));
+            list.Add(Convert.ToByte(0xC9));
             list.Add(Convert.ToByte(0x00));
-            list.Add(Convert.ToByte(0x08));
             list.Add(Convert.ToByte(0xC8));
             list.Add(Convert.ToByte(0x00));
             list.Add(Convert.ToByte(0x0A));
-            mut.ReleaseMutex();
-            Debug.WriteLine("Read");
-            this.BeginInvoke(new ShowSerialData(LineReceived), list);
+
+            this.BeginInvoke(new ShowSerialData(convertList));
 
         }
 
-        private void SerialDataReceivedEventHandler(object sender, SerialDataReceivedEventArgs e)
+        private void SerialInterface_OnDataReceived(byte[] data)
         {
+            foreach (Byte value in data)
             {
-                try
-                {
-                    SerialPort sp = (SerialPort)sender;
-                    while (sp.BytesToRead > 0)
-                    {
-                        mut.WaitOne();
-                        list.Add(Convert.ToByte(sp.ReadByte()));
-                        mut.ReleaseMutex();
-                    }
-                }
-                catch
-                {
-
-                }
-                this.BeginInvoke(new ShowSerialData(LineReceived), new object[] { list });
+                list.Add(Convert.ToByte(value));
             }
+
+            convertList();
+
         }
 
-        private void LineReceived(List<byte> _serialValue)
+        private void convertList()
         {
-            ushort nextValueUint = 0;
             short nextValueAD = 0;
             ushort nextValuePIX = 0;
             byte[] byteArray = new byte[4];
 
-            // while (_serialValue.Count >= 5 && _serialValue.IndexOf(0x0A) == 4)
-            try
+            while (list.Count >= 5)
             {
-                while (_serialValue.Count >= 5)
+                try
                 {
-                    if (_serialValue.IndexOf(0x0A) != 4)
+                    if (Convert.ToByte(list[4]) != 0x0A)
                     {
                         throw new Exception("LF not found");
                     }
-                    //Debug.WriteLine(_serialValue.IndexOf(0x0A));
-                    byteArray = _serialValue.GetRange(0, 4).ToArray();
-                    nextValueUint = BitConverter.ToUInt16(byteArray, 0);
+
+                    byteArray = list.GetRange(0, 4).ToArray();
+                    list.RemoveRange(0, 5);
                     BitArray nextValueADBit = new BitArray(byteArray);
                     if (nextValueADBit[11] == true)
                     {
-                        nextValueADBit = nextValueADBit.Or(new BitArray(System.BitConverter.GetBytes(0xF000)));
+                        nextValueADBit = nextValueADBit.Or(new BitArray(System.BitConverter.GetBytes(0xF0000000)));
                     }
                     nextValueADBit.CopyTo(byteArray, 0);
                     nextValueAD = BitConverter.ToInt16(byteArray, 0);
                     nextValuePIX = BitConverter.ToUInt16(byteArray, 2);
-
-                    Debug.WriteLine(nextValueAD);
-                    //Debug.WriteLine(nextValuePIX);
-
-                    //TimeSpan elapsedTime = new TimeSpan(DateTime.Now.Ticks - beginTime.Ticks);
-
-                    //debugTextbox.Text = elapsedTime.Milliseconds.ToString() + "/ " + nextValueAD.ToString() + "/ " + nextValuePIX.ToString();
-
-                    mut.WaitOne();
-                    _serialValue.RemoveRange(0, 5);
-                    //Debug.WriteLine(_serialValue.Count);
-                    mut.ReleaseMutex();
 
                     nextValueIndex = (nextValueIndex < liveDataAD.Length - 1) ? nextValueIndex + 1 : 0;
 
@@ -286,20 +170,19 @@ namespace plotBrembs
                     liveDataPIX[nextValueIndex] = Convert.ToDouble(nextValuePIX);
 
                     //beginTime = DateTime.Now;
+                    
+
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.ToString);
                 }
             }
-            catch (Exception ex)
-            {
-                _serialClear++;
-                Debug.WriteLine(_serialClear);
-                _serialValue.Clear();
-            }
 
 
-            //Debug.WriteLine(_serialValue.Count);
             updateData();
-
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -319,44 +202,22 @@ namespace plotBrembs
             MessageBox.Show(threads);
         }
 
-        private void SetTimer()
-        {
-            aTimer = new Timers.Timer(250);
-            aTimer.Elapsed += this.OnTimedEvent;
-            aTimer.AutoReset = true;
-            aTimer.SynchronizingObject = this;
-            aTimer.Enabled = false;
-        }
 
         private void updateData()
         {
-            //formsPlot1.Refresh();
+            if (chart1.InvokeRequired)
+            {
+                chart1.Invoke(new MethodInvoker(updateData));
+            }
+            else
+            {
+                chart1.Series["liveDataAD"].Points.RemoveAt(nextValueIndex);
+                chart1.Series["liveDataPIX"].Points.RemoveAt(nextValueIndex);
+                chart1.Series["liveDataAD"].Points.InsertY(nextValueIndex, liveDataAD[nextValueIndex]);
+                chart1.Series["liveDataPIX"].Points.InsertY(nextValueIndex, liveDataPIX[nextValueIndex]);
 
-            chart1.Series["liveDataAD"].Points.RemoveAt(nextValueIndex);
-            chart1.Series["liveDataPIX"].Points.RemoveAt(nextValueIndex);
-            chart1.Series["liveDataAD"].Points.InsertY(nextValueIndex, liveDataAD[nextValueIndex]);
-            chart1.Series["liveDataPIX"].Points.InsertY(nextValueIndex, liveDataPIX[nextValueIndex]);
-
-            Debug.WriteLine(liveDataAD[nextValueIndex].ToString() + " " + liveDataPIX[nextValueIndex].ToString());
-        }
-
-        private void OnTimedEvent(Object source, Timers.ElapsedEventArgs e)
-        {
-#if DEBUG
-            Stopwatch timer = new Stopwatch();
-            timer.Start();
-#endif
-            //formsPlot1.Plot.AxisAutoY(0.1, 0);
-            //formsPlot1.Plot.AxisAutoY(0.1, 1);
-            //Debug.WriteLine(formsPlot1.Plot.GetAxisLimits().ToString());
-            //formsPlot1.Refresh();
-            TimeSpan elapsedTime = new TimeSpan(DateTime.Now.Ticks - beginTime.Ticks);
-            //debugTextbox.Text = elapsedTime.Milliseconds.ToString() + " " + nextValueIndex.ToString() + " " + _serialClear.ToString();
-            beginTime = DateTime.Now;
-#if DEBUG
-            timer.Stop();
-            Debug.WriteLine("Render Taken: " + timer.Elapsed.TotalMilliseconds.ToString("#,##0.00 'milliseconds'"));
-#endif
+                Debug.WriteLine(liveDataAD[nextValueIndex].ToString() + " " + liveDataPIX[nextValueIndex].ToString());
+            }
 
         }
 
@@ -370,31 +231,7 @@ namespace plotBrembs
             //messageBoxCS.AppendLine();
             //MessageBox.Show(messageBoxCS.ToString(), "FormClosing Event");
 
-            try
-            {
-                if (_serialPort.IsOpen)
-                {
-                    Thread.Sleep(100);
-                    if (_serialPort.BytesToRead > 0)
-                    {
-                        _serialPort.WriteLine("Stop");
-                        _serialPort.Close();
-                    }
-                }
-                else
-                {
-
-                }
-
-                if (aTimer.Enabled == true)
-                {
-                    aTimer.Stop();
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString(), "SerialInterface Event");
-            }
+            serialCom.closePort();
         }
 
     }
---
+++ serialInterface.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace plotBrembs
+{
+    internal class serialInterface
+    {
+        public SerialPort _serialPort = null;
+
+        public delegate void DataReceivedHandler(byte[] data);
+        public event DataReceivedHandler OnDataReceived;
+
+        public serialInterface()
+        {
+            _serialPort = new SerialPort();
+            _serialPort.BaudRate = 115200;
+            _serialPort.Parity = Parity.None;
+        }
+
+        public void openPort(string port)
+        {
+            try
+            {
+                if (!_serialPort.IsOpen)
+                {
+                    _serialPort.PortName = port;
+                    _serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialDataReceivedEventHandler);
+                    _serialPort.Open();
+                    if (_serialPort.BytesToRead == 0)
+                    {
+                        _serialPort.WriteLine("S");
+                        _serialPort.WriteLine("S");
+                    }
+                    else
+                    {
+                        _serialPort.WriteLine("S");
+                    }
+
+                }
+                else
+                {
+                    _serialPort.WriteLine("S");
+                    _serialPort.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Serial failed", "Opening SerialPort Event");
+            }
+        }
+
+        public void closePort()
+        {
+            try
+            {
+                if (_serialPort.IsOpen)
+                {
+                    Thread.Sleep(100);
+                    if (_serialPort.BytesToRead > 0)
+                    {
+                        _serialPort.WriteLine("Stop");
+                        _serialPort.Close();
+                    }
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "SerialInterface Event");
+            }
+        }
+
+        private void SerialDataReceivedEventHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            {
+                SerialPort sp = (SerialPort)sender;
+                int bytesToRead = sp.BytesToRead;
+                byte[] buffer = new byte[bytesToRead];
+                sp.Read(buffer, 0, bytesToRead);
+
+                // Raise the OnDataReceived event
+                OnDataReceived?.Invoke(buffer);
+            }
+        }
+    }
+}
